e36056fae50811eada1ae30a8e836f1d3842ac35
sbaecker2005 <144857264+sbaecker2005@users.noreply.github.com>
2024-04-12 21:51:40 -0300

    Update main.c

--- main.c
+++ main.c
@@ -2,11 +2,11 @@
 #include "tarefas.h"
 
 int main(){
-    funcao fs[] = {criar, deletar, listar, salvar, carregar};
+    funcao fs[] = {criar, deletar, listar, exportar, salvar}; 
 
     Tarefa tarefas[TOTAL];
     int pos;
-    ERROS erro = fs[4](tarefas, &pos);
+    ERROS erro = gerenciarArquivo(tarefas, &pos, "tarefas.bin", 2); 
     if(erro != OK)
         pos = 0;
 
@@ -16,19 +16,26 @@ int main(){
         printf("1 - Criar tarefa\n");
         printf("2 - Deletar tarefa\n");
         printf("3 - Listar tarefas\n");
+        printf("4 - Exportar tarefas para arquivo de texto\n"); 
+        printf("5 - Salvar tarefas em arquivo binário\n"); 
         printf("0 - Sair\n");
         printf("Escolha uma opcao: ");
 
         scanf("%d", &opcao);
+        clearBuffer(); 
         opcao--;
-        if(opcao > 2)
+        if(opcao > 5) 
             printf("Opcao invalida\n");
-        else if(opcao >= 0)
+        else if(opcao >= 0 && opcao <= 5)
             fs[opcao](tarefas, &pos);
-        else
+        else if (opcao == 5) {
+            erro = gerenciarArquivo(tarefas, &pos, "tarefas.bin", 1); 
+            if (erro != OK)
+                printf("Erro ao salvar tarefas.\n");
+        } else
             printf("Sair...\n");
 
-    } while(opcao >= 0);
+    } while(opcao >= 0 && opcao <= 5);
 
-    fs[3](tarefas, &pos);
+    return 0;
 }
